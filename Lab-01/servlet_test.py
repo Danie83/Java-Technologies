63bd7461284f7fa07ca199fcf185d30112cdae41
Danie83 <danut_fili2017@yahoo.com>
2022-10-04 14:56:19 +0300

    Update servlet_test.py

--- Lab-01/servlet_test.py
+++ Lab-01/servlet_test.py
@@ -1,12 +1,20 @@
 import requests
+import random
+import string
+
+
+def get_random_string(length):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(length))
 
 def servlet_test(string, size, access):
     url = "http://localhost:8080/Compulsory/bonus?parameter=" + string + "&size=" + size + "&access=" + access
     response = requests.get(url)
 
 if "__main__" == __name__:
-    for i in range(1, 10):
-        if i % 2 == 0:
-            servlet_test("aavj", "0", "yes")
+    for i in range(1, 100):
+        if i % 5 == 0:
+            servlet_test(get_random_string(random.randint(5, 10)), "0", "yes")
         else:
-            servlet_test("aavj", "4", "no")
+            stringValue = get_random_string(random.randint(5, 10))
+            servlet_test(stringValue, str(random.randint(1, len(stringValue))), "no")
