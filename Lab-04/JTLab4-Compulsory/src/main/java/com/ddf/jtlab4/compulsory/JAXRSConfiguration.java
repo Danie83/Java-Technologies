486c5e19e385858fba83dd3a94af0cb702dd3dbc
Danie83 <danut_fili2017@yahoo.com>
2022-10-25 21:40:29 +0300

    update lab4 (connection pool works now)

---
+++ Lab-04/JTLab4-Compulsory/src/main/java/com/ddf/jtlab4/compulsory/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package com.ddf.jtlab4.compulsory;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
